be0e7fa2feae1d4d09f1a211ca35226b0a92ae97
jyc02 <jchin2250@gmail.com>
2024-02-03 16:40:09 -0600

    animations and textures

--- src/App.js
+++ src/App.js
@@ -1,40 +1,72 @@
-import React from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { Suspense } from "react";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import * as THREE from "three";
 import planetData from "./planetData";
+import sunTexture from "./textures/sun.jpg";
 import "./styles.css";
-import Header from "./components/Header/Header";
 
 export default function App() {
   return (
     <>
-      <Header>jjjj</Header>
-      <Canvas camera={{ position: [0, 20, 25], fov: 90 }}>
-        <Sun />
-        {planetData.map((planet) => (
-          <Planet planet={planet} key={planet.id} />
-        ))}
-        <Lights />
-        <OrbitControls />
+      <a
+        href="https://medium.com/geekculture/build-3d-apps-with-react-animated-solar-system-part-2-1186a5c8bd1"
+        className="article-link"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Step by step guide to how I build this
+      </a>
+      <Canvas camera={{ position: [0, 20, 25], fov: 45 }}>
+        <Suspense fallback={null}>
+          <Sun />
+          {planetData.map((planet) => (
+            <Planet planet={planet} key={planet.id} />
+          ))}
+          <Lights />
+          <OrbitControls />
+        </Suspense>
       </Canvas>
     </>
   );
 }
 function Sun() {
+  const texture = useLoader(THREE.TextureLoader, sunTexture);
   return (
     <mesh>
       <sphereGeometry args={[2.5, 32, 32]} />
-      <meshStandardMaterial color="#E1DC59" />
+      <meshStandardMaterial map={texture} />
     </mesh>
   );
 }
-function Planet({ planet: { color, xRadius, zRadius, size } }) {
+function Planet({
+  planet: {
+    color,
+    xRadius,
+    zRadius,
+    size,
+    speed,
+    offset,
+    rotationSpeed,
+    textureMap
+  }
+}) {
+  const planetRef = React.useRef();
+  const texture = useLoader(THREE.TextureLoader, textureMap);
+  useFrame(({ clock }) => {
+    const t = clock.getElapsedTime() * speed + offset;
+    const x = xRadius * Math.sin(t);
+    const z = zRadius * Math.cos(t);
+    planetRef.current.position.x = x;
+    planetRef.current.position.z = z;
+    planetRef.current.rotation.y += rotationSpeed;
+  });
+
   return (
     <>
-      <mesh position={[xRadius, 0, 0]}>
+      <mesh ref={planetRef}>
         <sphereGeometry args={[size, 32, 32]} />
-        <meshStandardMaterial color={color} />
+        <meshStandardMaterial map={texture} />
       </mesh>
       <Ecliptic xRadius={xRadius} zRadius={zRadius} />
     </>
@@ -64,7 +96,7 @@ function Ecliptic({ xRadius = 1, zRadius = 1 }) {
   const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
   return (
     <line geometry={lineGeometry}>
-      <lineBasicMaterial attach="material" color="#BFBBDA" linewidth={10} />
+      <lineBasicMaterial attach="material" color="#393e46" linewidth={10} />
     </line>
   );
 }
